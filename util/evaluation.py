a0323eecbc26b083f50a56fa9e6166684b681830
Lakshay-agarwal <32712374+Lakshay-agarwal@users.noreply.github.com>
2020-11-21 00:48:46 +0530

    Update evaluation.py

--- util/evaluation.py
+++ util/evaluation.py
@@ -51,24 +51,44 @@ def get_segmentation_names(seg_folder, patient_names_file):
 def dice_of_brats_data_set(gt_names, seg_names, type_idx):
     assert(len(gt_names) == len(seg_names))
     dice_all_data = []
+    sensitivity_all_data = []
+    specificity_all_data = []
     for i in range(len(gt_names)):
         g_volume = load_3d_volume_as_array(gt_names[i])
         s_volume = load_3d_volume_as_array(seg_names[i])
         dice_one_volume = []
+        sensitivity_one_volume = []
+        specificity_one_volume = []
         if(type_idx ==0): # whole tumor
             temp_dice = binary_dice3d(s_volume > 0, g_volume > 0)
             dice_one_volume = [temp_dice]
+            temp_sensitivity = sensitivity(s_volume > 0, g_volume > 0)
+            sensitivity_one_volume = [temp_sensitivity]
+            temp_specificity = specificity(s_volume > 0, g_volume > 0)
+            specificity_one_volume = [temp_specificity]
         elif(type_idx == 1): # tumor core
             s_volume[s_volume == 2] = 0
             g_volume[g_volume == 2] = 0
             temp_dice = binary_dice3d(s_volume > 0, g_volume > 0)
             dice_one_volume = [temp_dice]
+            temp_sensitivity = sensitivity(s_volume > 0, g_volume > 0)
+            sensitivity_one_volume = [temp_sensitivity]
+            temp_specificity = specificity(s_volume > 0, g_volume > 0)
+            specificity_one_volume = [temp_specificity]
         else:
             for label in [1, 2, 3, 4]: # dice of each class
                 temp_dice = binary_dice3d(s_volume == label, g_volume == label)
                 dice_one_volume.append(temp_dice)
+                temp_sensitivity = sensitivity(s_volume == label, g_volume == label)
+                sensitivity_one_volume.append(temp_sensitivity)
+                temp_specificity = specificity(s_volume == label, g_volume == label)
+                specificity_one_volume.append(temp_specificity)
+                
         dice_all_data.append(dice_one_volume)
-    return dice_all_data
+        sensitivity_all_data.append(sensitivity_one_volume)
+        specificity_all_data.append(specificity_one_volume)
+        
+    return dice_all_data, sensitivity_all_data, specificity_all_data
     
 if __name__ == '__main__':
     year = 15 # or 17
@@ -86,14 +106,26 @@ if __name__ == '__main__':
     gt_names  = get_ground_truth_names(g_folder, patient_names_file, year)
     seg_names = get_segmentation_names(s_folder, patient_names_file)
     for type_idx in range(3):
-        dice = dice_of_brats_data_set(gt_names, seg_names, type_idx)
+        dice,sensitivity,specificity = dice_of_brats_data_set(gt_names, seg_names, type_idx)
         dice = np.asarray(dice)
         dice_mean = dice.mean(axis = 0)
         dice_std  = dice.std(axis  = 0)
+        sensitivity = np.asarray(sensitivity)
+        sensitivity_mean = sensitivity.mean(axis = 0)
+        sensitivity_std = sensitivity.std(axis = 0)
+        specificity = np.asarray(specificity)
+        specificity_mean = specificity.mean(axis = 0)
+        specificity_std = specificity.std(axis = 0)
         test_type = test_types[type_idx]
         np.savetxt(s_folder + '/dice_{0:}.txt'.format(test_type), dice)
         np.savetxt(s_folder + '/dice_{0:}_mean.txt'.format(test_type), dice_mean)
         np.savetxt(s_folder + '/dice_{0:}_std.txt'.format(test_type), dice_std)
+        np.savetxt(s_folder + '/sensitivity_{0:}.txt'.format(test_type), sensitivity)
+        np.savetxt(s_folder + '/sensitivity_{0:}_mean.txt'.format(test_type), sensitivity_mean)
+        np.savetxt(s_folder + '/sensitivity_{0:}_std.txt'.format(test_type), sensitivity_std)
+        np.savetxt(s_folder + '/specificity_{0:}.txt'.format(test_type), specificity)
+        np.savetxt(s_folder + '/specificity_{0:}_mean.txt'.format(test_type), specificity_mean)
+        np.savetxt(s_folder + '/specificity_{0:}_std.txt'.format(test_type), specificity_std)
         print('tissue type', test_type)
         if(test_type == 'all'):
             print('tissue label', [1, 2, 3, 4])
