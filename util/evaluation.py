f864157cf48a0126d01f6995240fb45c2129033b
Lakshay-agarwal <32712374+Lakshay-agarwal@users.noreply.github.com>
2020-11-21 19:47:22 +0530

    Update evaluation.py

--- util/evaluation.py
+++ util/evaluation.py
@@ -13,7 +13,7 @@ import os
 import sys
 sys.path.append('./')
 import numpy as np
-from util.data_process import load_3d_volume_as_array, binary_dice3d
+from data_process import load_3d_volume_as_array, binary_dice3d, sensitivity, specificity, accuracy
 
 def get_ground_truth_names(g_folder, patient_names_file, year = 15):
     assert(year==15 or year == 17)
@@ -53,12 +53,14 @@ def dice_of_brats_data_set(gt_names, seg_names, type_idx):
     dice_all_data = []
     sensitivity_all_data = []
     specificity_all_data = []
+    acc_all_data = []
     for i in range(len(gt_names)):
         g_volume = load_3d_volume_as_array(gt_names[i])
         s_volume = load_3d_volume_as_array(seg_names[i])
         dice_one_volume = []
         sensitivity_one_volume = []
         specificity_one_volume = []
+        acc_one_volume = []
         if(type_idx ==0): # whole tumor
             temp_dice = binary_dice3d(s_volume > 0, g_volume > 0)
             dice_one_volume = [temp_dice]
@@ -66,6 +68,8 @@ def dice_of_brats_data_set(gt_names, seg_names, type_idx):
             sensitivity_one_volume = [temp_sensitivity]
             temp_specificity = specificity(s_volume > 0, g_volume > 0)
             specificity_one_volume = [temp_specificity]
+            temp_acc = accuracy(s_volume > 0, g_volume > 0)
+            acc_one_volume = [temp_acc]
         elif(type_idx == 1): # tumor core
             s_volume[s_volume == 2] = 0
             g_volume[g_volume == 2] = 0
@@ -75,6 +79,8 @@ def dice_of_brats_data_set(gt_names, seg_names, type_idx):
             sensitivity_one_volume = [temp_sensitivity]
             temp_specificity = specificity(s_volume > 0, g_volume > 0)
             specificity_one_volume = [temp_specificity]
+            temp_acc = accuracy(s_volume > 0, g_volume > 0)
+            acc_one_volume = [temp_acc]
         else:
             for label in [1, 2, 3, 4]: # dice of each class
                 temp_dice = binary_dice3d(s_volume == label, g_volume == label)
@@ -83,12 +89,15 @@ def dice_of_brats_data_set(gt_names, seg_names, type_idx):
                 sensitivity_one_volume.append(temp_sensitivity)
                 temp_specificity = specificity(s_volume == label, g_volume == label)
                 specificity_one_volume.append(temp_specificity)
+                temp_acc = accuracy(s_volume == label, g_volume == label)
+                acc_one_volume.append(temp_acc)
                 
         dice_all_data.append(dice_one_volume)
         sensitivity_all_data.append(sensitivity_one_volume)
         specificity_all_data.append(specificity_one_volume)
+        acc_all_data.append(acc_one_volume)
         
-    return dice_all_data, sensitivity_all_data, specificity_all_data
+    return dice_all_data, sensitivity_all_data, specificity_all_data, acc_all_data
     
 if __name__ == '__main__':
     year = 15 # or 17
@@ -106,7 +115,7 @@ if __name__ == '__main__':
     gt_names  = get_ground_truth_names(g_folder, patient_names_file, year)
     seg_names = get_segmentation_names(s_folder, patient_names_file)
     for type_idx in range(3):
-        dice,sensitivity,specificity = dice_of_brats_data_set(gt_names, seg_names, type_idx)
+        dice,sensitivity,specificity,acc = dice_of_brats_data_set(gt_names, seg_names, type_idx)
         dice = np.asarray(dice)
         dice_mean = dice.mean(axis = 0)
         dice_std  = dice.std(axis  = 0)
@@ -116,6 +125,9 @@ if __name__ == '__main__':
         specificity = np.asarray(specificity)
         specificity_mean = specificity.mean(axis = 0)
         specificity_std = specificity.std(axis = 0)
+        acc = np.asarray(acc)
+        acc_mean = acc.mean(axis = 0)
+        acc_std = acc.std(axis = 0)
         test_type = test_types[type_idx]
         np.savetxt(s_folder + '/dice_{0:}.txt'.format(test_type), dice)
         np.savetxt(s_folder + '/dice_{0:}_mean.txt'.format(test_type), dice_mean)
@@ -126,6 +138,9 @@ if __name__ == '__main__':
         np.savetxt(s_folder + '/specificity_{0:}.txt'.format(test_type), specificity)
         np.savetxt(s_folder + '/specificity_{0:}_mean.txt'.format(test_type), specificity_mean)
         np.savetxt(s_folder + '/specificity_{0:}_std.txt'.format(test_type), specificity_std)
+        np.savetxt(s_folder + '/accuracy_{0:}.txt'.format(test_type), acc)
+        np.savetxt(s_folder + '/accuracy_{0:}_mean.txt'.format(test_type), acc_mean)
+        np.savetxt(s_folder + '/accuracy_{0:}_std.txt'.format(test_type), acc_std)
         print('tissue type', test_type)
         if(test_type == 'all'):
             print('tissue label', [1, 2, 3, 4])
